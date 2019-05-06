590aa5a7f9e2fba6c4b2db155db9cf47d8ec90aa
Kheronn <kheronn@gmail.com>
2019-05-06 11:51:25 -0300

    Implementação da guarda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
+import { AuthguardService } from './services/authguard.service';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/public/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'admin/painel', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'admin/painel',  loadChildren: './components/admin/painel/painel.module#PainelModule'  },
-  { path: 'admin/funcionario',  loadChildren: './components/admin/funcionario/funcionario.module#FuncionarioModule'  },
-  { path: 'admin/departamento',  loadChildren: './components/admin/departamento/departamento.module#DepartamentoModule'  },
+  { path: 'admin/painel', loadChildren: './components/admin/painel/painel.module#PainelModule', canActivate: [AuthguardService]},
+  { path: 'admin/funcionario', loadChildren: './components/admin/funcionario/funcionario.module#FuncionarioModule', canActivate: [AuthguardService]},
+  { path: 'admin/departamento', loadChildren: './components/admin/departamento/departamento.module#DepartamentoModule', canActivate: [AuthguardService] },
 
 ];
 
---
+++ src/app/services/authguard.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { take, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthguardService implements CanActivate {
+
+
+
+  constructor(private afAuth: AngularFireAuth,
+    private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.afAuth.authState.pipe(
+      take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        if (!loggedIn) {
+          this.router.navigate(['/login']);
+        }
+      })
+
+    )
+  }
+
+}
