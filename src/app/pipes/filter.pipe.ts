3f21b738520f11c732dd93f215f331930b209c17
Kheronn <kheronn@gmail.com>
2019-05-10 10:53:04 -0300

    Ajustes no componente funcionario

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -8,9 +8,7 @@ export class FilterDepartamentoPipe implements PipeTransform {
   transform(value: any, filtro: any): any {
     if (filtro == 'TODOS') return value;
     if (value) {
-      if (!value) return [];
-      if (!value || value.length === 0) return value;
-      return value.filter(it => (it.departamento.nome === filtro))
+       return value.filter(elem => (elem.departamento.nome === filtro))
     }
   }
 }
