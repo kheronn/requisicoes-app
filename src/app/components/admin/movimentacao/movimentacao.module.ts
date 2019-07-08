75df991922364c5512e49b6afa4c6e613aca1054
Kheronn <kheronn@gmail.com>
2019-07-08 07:48:31 -0300

    Refactor class

--- src/app/components/admin/movimentacao/movimentacao.module.ts
+++ src/app/components/admin/movimentacao/movimentacao.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MovimentacaoRoutingModule } from './movimentacao-routing.module';
-import { MovimentacaoComponent } from './movimentacao.component';
 import { ComumModule } from 'src/app/modules/comum.module';
 import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
-  declarations: [MovimentacaoComponent],
+  declarations: [],
   imports: [
     ComumModule,
     MovimentacaoRoutingModule,
