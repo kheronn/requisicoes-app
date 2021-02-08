fca35232f8547af8b0696caaaf49530ba9bad966
Kheronn <kheronn@gmail.com>
2021-02-08 15:42:18 -0300

    Update: Tema do PrimeNG

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "SUA-CHAVE",
-    authDomain: "SUA-CHAVE",
-    databaseURL: "SUA-CHAVE",
-    projectId: "SUA-CHAVE",
-    storageBucket: "SUA-CHAVE",
-    messagingSenderId: "SUA-CHAVE"
+    apiKey: "AIzaSyCng_cba2SpK1MVIl478IUInfE5v2UvIxU",
+    authDomain: "requisicoes-app.firebaseapp.com",
+    databaseURL: "https://requisicoes-app.firebaseio.com",
+    projectId: "requisicoes-app",
+    storageBucket: "requisicoes-app.appspot.com",
+    messagingSenderId: "192989744894",
+    appId: "1:192989744894:web:5fe8a9c0b724f4863fdfaf"
 
   }
 };
