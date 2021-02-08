c20a0e3933cedbc922149f62d84b7af1721f7d6d
Kheronn <kheronn@gmail.com>
2021-02-08 15:50:20 -0300

    Update: Import dos modulos do PrimeNG

--- src/app/modules/primeng.modules.ts
+++ src/app/modules/primeng.modules.ts
@@ -1,30 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonModule } from 'primeng/components/button/button';
-import { FieldsetModule } from 'primeng/components/fieldset/fieldset';
+import { ButtonModule } from 'primeng//button';
+import { FieldsetModule } from 'primeng/fieldset';
 import {InputMaskModule} from 'primeng/inputmask';
-import { MessagesModule } from 'primeng/components/messages/messages';
-import { DataTableModule } from 'primeng/components/datatable/datatable';
-import { DialogModule } from 'primeng/components/dialog/dialog';
-import { InputTextModule } from 'primeng/components/inputtext/inputtext';
-import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import { MessagesModule } from 'primeng/messages';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 import {DropdownModule} from 'primeng/dropdown';
 import {ToggleButtonModule} from 'primeng/togglebutton';
-import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import {CheckboxModule} from 'primeng/checkbox';
-import {CalendarModule} from 'primeng/components/calendar/calendar';
-import {TabViewModule} from 'primeng/components/tabview/tabview';
+import {CalendarModule} from 'primeng/calendar';
+import {TabViewModule} from 'primeng/tabview';
 
 
 @NgModule({
   imports: [
     CommonModule,ButtonModule,FieldsetModule, InputMaskModule,MessagesModule, CheckboxModule ,
-    DataTableModule,DialogModule,InputTextModule,InputTextareaModule,DropdownModule,
+    DialogModule,InputTextModule,InputTextareaModule,DropdownModule,
     ConfirmDialogModule,CalendarModule,TabViewModule, ToggleButtonModule
   ],
   exports:[
     ButtonModule,FieldsetModule, InputMaskModule,MessagesModule, CheckboxModule ,
-    DataTableModule,DialogModule,InputTextModule,InputTextareaModule,DropdownModule,
+    DialogModule,InputTextModule,InputTextareaModule,DropdownModule,
     ConfirmDialogModule,CalendarModule,TabViewModule, ToggleButtonModule
 
   ],
