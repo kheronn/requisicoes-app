04fca176ea94042ef1efb7d4717f89d7b305f550
Kheronn <kheronn@gmail.com>
2021-02-08 16:40:03 -0300

    Update Funcionario: Nome das classes de alinhamento do formulário

--- src/app/services/requisicao.service.ts
+++ src/app/services/requisicao.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ServiceFirebase } from '../core/servicefirebase.service';
 import { Requisicao } from '../models/requisicao.model';
 import { AngularFirestore } from '@angular/fire/firestore';
-import * as firebase from "firebase/app";
+import  firebase from "firebase/app";
 
 @Injectable({
   providedIn: 'root'
